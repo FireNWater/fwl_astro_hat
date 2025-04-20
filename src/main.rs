5ef0921e8222b10a8432a260e096f14a8a28155e
FireNWater <klhoard@proton.me>
2025-04-20 19:12:03 +0000

    Can blink an Rpi pin now!!  Woot!!

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,24 @@
 
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::thread;
+use std::time::Duration;
+
+use rppal::gpio::Gpio;
+use rppal::system::DeviceInfo;
+
+// Gpio uses BCM pin numbering. 
+const GPIO_LED: u8 = 24;    // GPIO 24 is tied to physical pin 18
+
+
+fn main() -> Result<(), Box<dyn Error>> {
+    println!("Blinking LED {GPIO_LED} on a {}.", DeviceInfo::new()?.model());
+
+    let mut pin = Gpio::new()?.get(GPIO_LED)?.into_output();
+
+    // Blink the LED by setting the pin's logic level high for 500 ms.
+    pin.set_high();
+    thread::sleep(Duration::from_millis(500));
+    pin.set_low();
+
+    Ok(())
 }
