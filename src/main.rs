29696b26312a1565847ac66148700445fbd867b0
FireNWater <klhoard@proton.me>
2025-04-21 18:40:50 -0500

    Included structs

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,35 @@ use std::time::Duration;
 use rppal::gpio::Gpio;
 use rppal::system::DeviceInfo;
 
-// Gpio uses BCM pin numbering. 
-const GPIO_LED: u8 = 24;    // GPIO 24 is tied to physical pin 18
-
+struct  DCPorts {
+    port_1: u8,
+    port_2: u8,
+    port_3: u8,
+    port_extra: u8,
+}
+struct DewHeaters {
+    heater_1: u8,
+    heater_2: u8,
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
-    println!("Blinking LED {GPIO_LED} on a {}.", DeviceInfo::new()?.model());
 
-    let mut pin = Gpio::new()?.get(GPIO_LED)?.into_output();
+    // Gpio uses BCM pin numbering.
+    let dc = DCPorts {
+        port_1: 22,
+        port_2: 23,
+        port_3: 24,
+        port_extra: 25,
+    };
+
+    let dew_heaters = DewHeaters {
+        heater_1: 12,
+        heater_2: 13,
+    };
+
+    println!("Blinking LED {} on a {}.", dc.port_3, DeviceInfo::new()?.model());
+
+    let mut pin = Gpio::new()?.get(dc.port_3)?.into_output();
 
     // Blink the LED by setting the pin's logic level high for 500 ms.
     pin.set_high();
